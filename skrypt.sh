aeecc11dfee2e0c1e731f05deecd678c30ed63e2
Kacper Jasyk <kacper@MacBook-Air-Kacper.local>
2025-04-27 09:31:46 +0200

    Tworzenie 100 plikó∑ logów

--- skrypt.sh
+++ skrypt.sh
@@ -2,4 +2,10 @@
 
 if [ "$1" = "--date" ]; then
     date
+elif [ "$1" = "--logs" ]; then
+    for i in {1..100}; do
+        echo "log$i.txt" > "log$i.txt"
+        echo "Utworzony przez skrypt.sh" >> "log$i.txt"
+        echo "Data: $(date)" >> "log$i.txt"
+    done
 fi
