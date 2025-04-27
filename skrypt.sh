e1c1a15005e91f8c241775a4f5d4e407da28844c
Kacper Jasyk <kacper@MacBook-Air-Kacper.local>
2025-04-27 09:42:16 +0200

    Funkcja help

--- skrypt.sh
+++ skrypt.sh
@@ -3,9 +3,20 @@
 if [ "$1" = "--date" ]; then
     date
 elif [ "$1" = "--logs" ]; then
-    for i in {1..100}; do
+    count=100
+    if [ -n "$2" ]; then
+        count=$2
+    fi
+    
+    for i in $(seq 1 $count); do
         echo "log$i.txt" > "log$i.txt"
         echo "Utworzony przez skrypt.sh" >> "log$i.txt"
         echo "Data: $(date)" >> "log$i.txt"
     done
+elif [ "$1" = "--help" ]; then
+    echo "Dostępne opcje:"
+    echo "--date   - wyświetla dzisiejszą datę"
+    echo "--logs   - tworzy 100 plików logów"
+    echo "--logs N - tworzy N plików logów"
+    echo "--help   - wyświetla pomoc"
 fi
