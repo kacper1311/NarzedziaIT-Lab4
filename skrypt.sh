3813f28b87784f1abf92498851fdc9cbc0b19ef1
Kacper Jasyk <kacper@MacBook-Air-Kacper.local>
2025-04-27 10:57:56 +0200

    Aktualizacja help

--- skrypt.sh
+++ skrypt.sh
@@ -15,8 +15,11 @@ elif [ "$1" = "--logs" ] || [ "$1" = "-l" ]; then
     done
 elif [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
     echo "Dostępne opcje:"
-    echo "--date, -d   - wyświetla dzisiejszą datę"
-    echo "--logs, -l   - tworzy 100 plików logów"
-    echo "--logs N, -l N - tworzy N plików logów"
-    echo "--help, -h   - wyświetla pomoc"
+    echo "--date, -d       - wyświetla dzisiejszą datę"
+    echo "--logs, -l       - tworzy 100 plików logów"
+    echo "--logs N, -l N   - tworzy N plików logów"
+    echo "--init           - klonuje repozytorium i ustawia PATH"
+    echo "--error, -e      - tworzy 100 plików errorów"
+    echo "--error N, -e N  - tworzy N plików errorów"
+    echo "--help, -h       - wyświetla pomoc"
 fi
